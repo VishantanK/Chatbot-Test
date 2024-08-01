fd0686424f09ab63e49ac6bc02bf266f1a292d75
Vishantan Kumar <73944107+VishantanK@users.noreply.github.com>
2024-08-01 11:04:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def store_context(session_id: str, question: str, answer: str):
         context += f"\nUser: {question}\nBot: {answer}"
     else:
         context = f"User: {question}\nBot: {answer}"
-    redis_client.set(context_key, context, ex=1200)  # Set TTL to 24 hours
+    redis_client.set(context_key, context, ex=1800)  # Set TTL to 24 hours
 
 # Function to retrieve conversation context
 def get_context(session_id: str) -> str:
@@ -76,27 +76,52 @@ def get_context(session_id: str) -> str:
 def get_openai_llm(api_key, model, temperature, max_tokens):
     return ChatOpenAI(openai_api_key=api_key, model=model, temperature=temperature, max_tokens=max_tokens)
 
-
 # Define the prompt templates
 cypher_generation_prompt = PromptTemplate(
     template="""
-    You are an expert Neo4j Developer translating user questions into Cypher to answer questions about bioinformatics and biology from given Knowledge Graphs.
+    You are an expert Neo4j Developer translating user questions into Cypher to answer questions from given Knowledge Graphs. You will be given:
+    
+    1. The schema of the Knowledge Graph
+    2. The user question
+    3. Context in the form of previous question and bot response
 
     Instructions:
     - ONLY RETURN THE CYPHER QUERY
     - DO NOT INCLUDE ANY EXPLANATION, NATURAL LANGUAGE TEXT, OR CODE BLOCKS
-    - FOR DRUGGABILITY CHECK IF IT IS TRUE OR FALSE
+    - FOR Druggability CHECK IF IT IS TRUE OR FALSE.
     - MAKE COMPLICATED OR MULTISTEP QUERIES IF NEEDED
     - IF ASKED ABOUT RISK SOURCES OR HOW RISK SCORE IS CALCULATED CHECK THE 'Source' RELATIONSHIP PROPERTY 
         - EXAMPLE : "MATCH (g:Gene)-[r:HAS_RISK_SCORE]->(rs:Risk_Score) RETURN rs.score, r.Source"
     - IF SIGNIFICANCE OR STATISTICAL SIGNIFICANCE FOR GWAS OR SMR CHECK p-value < 0.05 FROM THE RELATIONSHIP PROPERTIES
-    - IF ASKED WETHER A PROTEIN IS AN "ENZYME" CHECK THE PROTEIN TYPE NODE. OTHER PROTEIN TYPES ARE "GPCR", "RECEPTOR", "ION CHANNEL" and "OTHER"
-    - Protein_Class CONTAINS INFO ON WETHER A PROTEIN IS KINASE, CYTOKINE, WHICH TYPE OF ENZYME ETC.
+    - IF ASKED WETHER A PROTEIN IS AN ENZYME CHECK THE Protein_Type NODE.
+    - Protein_Class CONTAINS INFO ON WETHER A PROTEIN IS KINASE, CYTOKINE BASICALLY SUBCATEGORY.
     - FOR SMR, GWAS, COLOC, SINGLE CELL DATA, FIRST CHECK IF THE GENE HAS THAT NODE AND THEN CHECK THE INFO RELATIONSHIP AND HIT NODES
     - FOR COLOC_pQTL IN THE COLOC_pQTL INFO RELATIONSHIP CIS_TRANS = TRUE MEANS CIS AND OTHERWISE MEANS FALSE
-    - MAKE SURE IF YOU QUERY A PATHWAY NAME OR CLASS OR ONTOLOGY NAME IT IS CASE-INDEPENDANT
+    - MAKE SURE IF YOU QUERY A PATHWAY NAME OR CLASS OR ONTOLOGY NAME, IT IS CASE-INDEPENDANT (ALWAYS CONVERT TO LOWER CASE TO CHECK)
     - DIRECTIONALITY IN GWAS, AND SMR IS BASED ON THE SIGN OF THE BETA-VALUE
+    - For SMR check both pQTL and OmicSynth SMR info unless specified
+    - Protein Class is connected to the Gene node with the BELONGS_TO_CLASS relationship
+    - Protein Type is connected to the Protein node with the IS_TYPE relationship.
+
     
+    Example Queries and Answers:
+    - "Which genes have a risk score > 4 and are druggable?" : MATCH (g:Gene)-[r1:HAS_RISK_SCORE]->(r:Risk_Score), (g)-[r2:IS_DRUGGABLE]->(d:Druggability) WHERE r.score > 4 AND d.druggability_query = TRUE RETURN g, r, d, r1, r2
+    - "Which genes among [gene_list] are enzymes?" : MATCH (g:Gene)-[r1:CODES]->(p:Protein)-[r2:IS_TYPE]->(pt:Protein_type) WHERE g1.Symbol IN [gene_list] RETURN g, p, pt, r1, r2, r3
+    - "Which genes are kinases?" : MATCH (g:Gene)-[r1:BELONGS_TO_CLASS]->(pc:Protein_Class) WHERE pc.protein_class = "Kinase" RETURN g, p, pc, r1
+    - "What are the pathways for genes having risk scores > 4?" : MATCH (g:Gene)-[r1:HAS_RISK_SCORE]->(r:Risk_Score), (g)-[r2:INVOLVED_IN_PATHWAY]->(p:Pathway) WHERE r.score > 4 RETURN g, p
+    - "What are the risk sources for genes?" : MATCH (g:Gene)-[r1:HAS_RISK_SCORE]->(r:Risk_Score) RETURN g, r, r1
+    "Colocalization and SMR info for gene X" : MATCH (g:Gene)
+        OPTIONAL MATCH (g)-[r1:HAS_eQTL_COLOCALIZATION_HIT]->(eqtl: eQTL_Coloc_Tissue)-[r2:eqTL_COLOC_INFO]->(eqtl_info: eQTL_Coloc_Hit)
+        OPTIONAL MATCH (g)-[r3:HAS_pQTL_COLOCALIZATION_HIT]->(pqtl: pQTL_Colocalization)-[r4:pQTL_COLOC_INFO]->(pqtl_info: pQTL_Coloc_Hit)
+        OPTIONAL MATCH (g)-[r5:HAS_pQTL_SMR_HIT]->(pqtl_smr: pQTL_SMR)-[r6:pQTL_SMR_INFO]->(pqtl_smr_info: pQTL_SMR_HIT)
+        OPTIONAL MATCH (g)-[r7:HAS_OMICSYNTH_SMR_HIT]->(omicsynth: OmicSynth_SMR)-[r8:Omicsynth_SMR_INFO]->(omicsynth_info: Omicsynth_SMR_HIT)
+        WHERE g.Symbol = "X" RETURN g, eqtl, pqtl, pqtl_smr, omicsynth, eqtl_info, pqtl_info, pqtl_smr_info, omicsynth_info
+    - "How many statistically significant GWAS hits does gene X have?" : MATCH (g:Gene)-[r1:HAS_GWAS_HIT]->(gw:GWAS)-[r2:GWAS_INFO]->(gw_hit:GWAS_HIT) WHERE (gw_hit.pval < 0.05 AND g.Symbol = "X") RETURN COUNT(gw_hit) AS GWAS_Hits
+    - "How many statistically significant SMR hits does gene X have?" : MATCH (g:Gene)
+        OPTIONAL MATCH (g)-[:HAS_pQTL_SMR_HIT]->(pqtl_smr: pQTL_SMR)-[:pQTL_SMR_INFO]->(pqtl_smr_info: pQTL_SMR_HIT)
+        OPTIONAL MATCH (g)-[:HAS_OMICSYNTH_SMR_HIT]->(omicsynth: OmicSynth_SMR)-[:Omicsynth_SMR_INFO]->(omicsynth_info: Omicsynth_SMR_HIT)
+        WHERE (pqtl_smr_info.p_smr < 0.05 OR omicsynth_info.p_smr < 0.05 AND g.Symbol = "X")
+        RETURN COUNT(DISTINCT pqtl_smr_info) + COUNT(DISTINCT omicsynth_info) AS Statistically_Significant_SMR_Hits
     Schema: {schema}
     Question: {question}
     """,
@@ -117,6 +142,21 @@ kg_generation_prompt = PromptTemplate(
     - WRITE COMPLEX CYPHER QUERIES WHICH EXTRACT ONLY THE INFORMATION NEEDED BUT DO NOT RETURN JUST THE PROPERTIES SINCE THEY CANT BE VISUALISED ON THE GRAPH
     - GENERATED CYPHER QUERIES SHOULD BE ABLE TO GENERATE GRAPH IN NEO4J
 
+     Example Queries and Answers:
+    - "Which genes have a risk score > 4 and are druggable?" : MATCH (g:Gene)-[r1:HAS_RISK_SCORE]->(r:Risk_Score), (g)-[r2:IS_DRUGGABLE]->(d:Druggability) WHERE r.score > 4 AND d.druggability_query = TRUE RETURN g, r, d, r1, r2
+    - "Which genes are enzymes?" : MATCH (g:Gene)-[r1:CODES]->(p:Protein)-[r2:IS_TYPE]->(pt:Protein_type), (g:Gene)-[r3:BELONGS_TO_CLASS]->(pc:Protein_Class) RETURN g, p, pt, pc, r1, r2, r3
+    - "Which genes are kinases?" : MATCH (g:Gene)-[r1:BELONGS_TO_CLASS]->(pc:Protein_Class) WHERE pc.protein_class = "Kinase" RETURN g, p, pc, r1
+    - "What are the pathways for genes having risk scores > 4?" : MATCH (g:Gene)-[r1:HAS_RISK_SCORE]->(r:Risk_Score), (g)-[r2:INVOLVED_IN_PATHWAY]->(p:Pathway) WHERE r.score > 4 RETURN g, p, r1, r2
+    - "What are the risk sources for genes?" : MATCH (g:Gene)-[r1:HAS_RISK_SCORE]->(r:Risk_Score) RETURN g, r, r1
+    "Colocalization and SMR info for gene X" : MATCH (g:Gene)
+        OPTIONAL MATCH (g)-[r1:HAS_eQTL_COLOCALIZATION_HIT]->(eqtl: eQTL_Coloc_Tissue)-[r2:eqTL_COLOC_INFO]->(eqtl_info: eQTL_Coloc_Hit)
+        OPTIONAL MATCH (g)-[r3:HAS_pQTL_COLOCALIZATION_HIT]->(pqtl: pQTL_Colocalization)-[r4:pQTL_COLOC_INFO]->(pqtl_info: pQTL_Coloc_Hit)
+        OPTIONAL MATCH (g)-[r5:HAS_pQTL_SMR_HIT]->(pqtl_smr: pQTL_SMR)-[r6:pQTL_SMR_INFO]->(pqtl_smr_info: pQTL_SMR_HIT)
+        OPTIONAL MATCH (g)-[r7:HAS_OMICSYNTH_SMR_HIT]->(omicsynth: OmicSynth_SMR)-[r8:Omicsynth_SMR_INFO]->(omicsynth_info: Omicsynth_SMR_HIT)
+        WHERE g.Symbol = "X" RETURN g, eqtl, pqtl, pqtl_smr, omicsynth, eqtl_info, pqtl_info, pqtl_smr_info, omicsynth_info, r1, r2, r3, r4, r5, r6, r7, r8
+    - "How many statistically significant GWAS hits does gene X have?" : MATCH (g:Gene)-[r1:HAS_GWAS_HIT]->(gw:GWAS)-[r2:GWAS_INFO]->(gw_hit:GWAS_HIT) WHERE (gw_hit.pval < 0.05 AND g.Symbol = "X") RETURN g, gw, gw_hit, r1, r2
+
+
     Schema: {schema}
     Question: {question}
     """,
@@ -126,12 +166,10 @@ kg_generation_prompt = PromptTemplate(
 compile_prompt = PromptTemplate(
     input_variables=["query", "results"],
     template="""
-    - You are a domain expert in bioinformatics and biology and will compile cypher query results into a single coherent answer given the original query: {query} and the following results from subqueries: {results}.
+    You are a domain expert in bioinformatics and have a lot of information about ALS. You are given the output that is produced after invoking a Cypher Query into an ALS knowledge Graph. The Cypher query was generated based off a user query: {query} and this is the output: {results}.
+    Analyze the outputs and give the user a relavant answer based on the query and the results.
     - DO NOT cite anything.
     - DO NOT include the original query in the answer.
-    - CAN use existing knowledge to compile the answer.
-    - If information is missing, answer based on your existing knowledge.
-    - PROVIDE DISCLAIMER if using existing knowledge.
     - Be concise and to the point.
     - Format the response using Markdown:
     - Use **bold** for important terms
@@ -141,8 +179,17 @@ compile_prompt = PromptTemplate(
         - GWAS hits
         - SMR : Can be either pQTL or eQTL sourced from different tissues and Data sources - GTex, Metabrain, Yang, UKB
         - Colocalization : Can be either pQTL or eQTL sourced from different tissues and Data sources Data sources - GTex, Metabrain, Yang, UKB
-    - DO NOT REPEAT THE ANSWER FROM THE CONTEXT
     
+    - ONLY USE EXISTING KNOWLEDGE IF THE INFORMATION IS MISSING IN THE RESULTS
+    - KEEP THE ADDITIONAL INFORMATION TO A MINIMUM
+    - IF PROVIDING INFORMATION FROM YOUR OWN KNOWLEDGE, STRUCTURE IT AS:
+        **Based on Query Results :**
+        Queried Result
+        **Based on Existing Knowledge :**
+        Existing Knowledge
+    
+        
+    The Information given in the context is just for *REFERENCE* DO NOT REPEAT content from the previous bot responses UNLESS RELEVANT.
     """
 )
 
@@ -156,7 +203,6 @@ with st.sidebar:
     temperature = st.slider("Temperature", min_value=0.0, max_value=0.5, value=0.01)
     generate_kg = st.checkbox("Generate Knowledge Graph")
 
-
 # Initialize LLMs
 llm4 = get_openai_llm(api_key, model, temperature, max_tokens)
 llm4mini = get_openai_llm(api_key, "gpt-4o-mini", temperature, max_tokens)
@@ -165,35 +211,20 @@ kg_chain = LLMChain(llm=llm4, prompt=kg_generation_prompt)
 compile_chain = LLMChain(llm=llm4mini, prompt=compile_prompt)
 schema = graph.schema
 
-# Function to visualize the graph
-def visualize_graph(results: List):
-    net = Network(height="500px", width="100%", directed=True)
-    for record in results:
-        for node in record.data():
-            net.add_node(node, label=node)
-        for node in record.data():
-            for rel in record.data()[node]:
-                net.add_edge(node, rel, label=rel)
-    net.show("graph.html")
-    HtmlFile = open("graph.html", "r", encoding="utf-8")
-    source_code = HtmlFile.read()
-    components.html(source_code, height=600)
 
 # Function to generate and execute Cypher queries
 def process_query(session_id: str, query: str, schema: str):
     cache_key = create_cache_key(query, schema)
-    cached_results = cache_results(cache_key)
-    if cached_results:
-        results = cached_results
-    else:
-        cypher_query = cypher_chain.run({"schema": schema, "question": query})
-        cypher_query = cypher_query.strip().replace("```cypher", "").replace("```", "").strip()
+    #cached_results = cache_results(cache_key)
 
-        with driver.session() as session:
-            results = session.run(cypher_query)
-            results = list(results)  # Convert to list to preserve Neo4j types
+    cypher_query = cypher_chain.run({"schema": schema, "question": query})
+    cypher_query = cypher_query.strip().replace("```cypher", "").replace("```", "").strip()
 
-        cache_results(cache_key, results)
+    with driver.session() as session:
+        results = session.run(cypher_query)
+        results = list(results)  # Convert to list to preserve Neo4j types
+
+    #cache_results(cache_key, results)
 
     final_answer_placeholder = st.empty()
     
@@ -208,18 +239,18 @@ def process_query(session_id: str, query: str, schema: str):
     
     final_response = generate_final_answer()
 
-    #st.subheader("Generated Cypher Query")
-    #st.code(cypher_query)
-
-    st.subheader("Query Results")
-    st.write(pd.DataFrame([record.data() for record in results]))
+    # Store the Cypher query in the session state
+    st.session_state.queries.append(cypher_query)
+    
+    st.subheader("Generated Cypher Query")
+    st.code(cypher_query)
 
     if generate_kg:
         kg_query = kg_chain.run({"schema": schema, "question": query})
         kg_query = kg_query.strip().replace("```cypher", "").replace("```", "").strip()
         st.subheader("Knowledge Graph Cypher Query")
         st.code(kg_query)
-        st.markdown("To visualise the KG, input the cypher query [here](https://browser.neo4j.io/?_gl=1*1mqlp1u*_gcl_aw*R0NMLjE3MjE0MTI1NzAuQ2owS0NRanctdUswQmhDMEFSSXNBTlF0Z0dPZHI2MFdBbUkxUmtFeXNfelNtdkFWOEZLajJSRWVMTjR2b1BZMGhGNUdjM3loQXo4R0wxWWFBamJyRUFMd193Y0I.*_gcl_au*ODQwMDI2NTMyLjE3MTg3Mjk5ODQ.*_ga*MTUzMjMxMzIyMS4xNzE4NzI5OTgz*_ga_DL38Q8KGQC*MTcyMTY2MTc0Ni4xMy4xLjE3MjE2NjE3NTMuMC4wLjA.*_ga_DZP8Z65KK4*MTcyMTY2MTc0Ni4yNC4xLjE3MjE2NjE3NTMuMC4wLjA.)", unsafe_allow_html=True)
+        st.markdown("To visualize the KG, input the cypher query [here](http://35.203.6.204:7474/browser/)", unsafe_allow_html=True)
 
     return final_response
 
@@ -227,13 +258,20 @@ def process_query(session_id: str, query: str, schema: str):
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+if "queries" not in st.session_state:
+    st.session_state.queries = []
+
 session_id = st.session_state.get('session_id', str(time.time()))
 st.session_state['session_id'] = session_id
 
-# Display chat messages
-for message in st.session_state.messages:
+# Display chat messages and Cypher queries
+for i, message in enumerate(st.session_state.messages):
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+        # Display the corresponding Cypher query if it exists
+        if message["role"] == "assistant" and i < len(st.session_state.queries):
+            st.subheader("Generated Cypher Query")
+            st.code(st.session_state.queries[i])
 
 # Chat input
 if prompt := st.chat_input("Ask a question about bioinformatics"):
@@ -243,7 +281,7 @@ if prompt := st.chat_input("Ask a question about bioinformatics"):
     
     with st.chat_message("assistant"):
         context = get_context(session_id)
-        full_response= process_query(session_id, f"Context: {context} \n Prompt: {prompt}", schema)
+        full_response = process_query(session_id, f"Context: {context} \n Prompt: {prompt}", schema)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
         store_context(session_id, prompt, full_response)
-        
+
